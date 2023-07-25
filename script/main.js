0eebcdc23d7bfea53ced4de7ceee4b90bbad7bcd
Jordan Godwin <jordan.godwin13@gmail.com>
2023-07-25 12:10:52 -0500

    Add code to ship JSON obj to serverside. NOTE: THIS IS CURRENTLY NON FUNCTIONAL. RESULTS IN 404.

--- script/main.js
+++ script/main.js
@@ -5,8 +5,22 @@ function handleSubmit(event) {
 
     const value = Object.fromEntries(data.entries());
 
-    console.log(JSON.stringify({ value }))
+    /*console.log(JSON.stringify({ value }))*/
+    let xhr = new XMLHttpRequest();
+    let url = "url?data=" + encodeURIComponent(JSON.stringify({value}));
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.email + ", " + json.password);
+    }
+};
+  xhr.send();
   }
 
   const form = document.getElementById('weightForm');
   form.addEventListener('submit', handleSubmit);
+
+  // Sending a receiving data in JSON format using GET method
+//      
