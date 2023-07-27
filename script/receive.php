faf8c5729a489b64e30b8094a15c91f926df39ea
Jordan Godwin <jordan.godwin13@gmail.com>
2023-07-27 12:51:50 -0500

    Change variable in receive script to match json variable.

--- script/receive.php
+++ script/receive.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json");
 // build a PHP variable from JSON sent using POST method
 $v = json_decode(stripslashes(file_get_contents("php://input")));
 // build a PHP variable from JSON sent using GET method
-$v = json_decode(stripslashes($_GET["data"]));
+$v = json_decode(stripslashes($_GET["value"]));
 // encode the PHP variable to JSON and send it back on client-side
 echo json_encode($v);
 ?>
