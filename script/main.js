3dea3c0f42216aea3adb8fa65ed30eae53b6fd06
Jordan Godwin <jordan.godwin13@gmail.com>
2023-08-02 08:43:41 -0500

    Add logs for debugging.

--- script/main.js
+++ script/main.js
@@ -31,11 +31,14 @@ function pullPreviousRecord(event){
     const formInput = document.getElementById('previousWeight');
     console.log(`Looking for something from ${dateValue}?`);
     let xhr = new XMLHttpRequest();
-    let url = 'script/receive.php?date=' + dateValue;
+    let url = 'script/records.php?date=' + dateValue;
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
+            const formInput = document.getElementById('previousWeight');
             formInput.innerHTML = xhr.responseText;
+            console.log('request came back.');
+            console.log(xhr.responseText);
         };
     };
     xhr.send();
